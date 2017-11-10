14465e4bb65fa92680c4e8be8ba3cbff69221387
Akanksha Adhikary <LADYHART@users.noreply.github.com>
2017-11-10 10:52:23 +0000

    html from example

---
+++ main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello PHP</title>
+</head>
+<body>
+<from>
+<label>What is your Name!</label>
+<input type="text" name="yourname" id="yourname" placeholder="Enter your name" width="128" height="24"/>
+<br/>
+</from>	
+</body>
+</html>
