bf6b83d0d58e111e9a9c5f7827350ab24a4cfdda
Akanksha Adhikary <LADYHART@users.noreply.github.com>
2017-11-10 03:02:30 -0800

    adding server post controls

--- main.php
+++ main.php
@@ -1,13 +1,20 @@
+<?php $yourname=""; 
+  $yourname = $_POST["yourname"];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <title>Hello PHP</title>
 </head>
 <body>
-<from>
+<form action="" method="post">
 <label>What is your Name!</label>
 <input type="text" name="yourname" id="yourname" placeholder="Enter your name" width="128" height="24"/>
 <br/>
-</from>	
+ <?php if(!empty($yourname)) { ?>
+  <h3>Welcome Mr. <?=$yourname ?>
+  <?php }>
+</form>	
 </body>
 </html>
